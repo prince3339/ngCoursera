1344b08f1b2607ce0b67459a479167b11d853970
Sabbir Ahmed <sabbir.ahmed@selise.com.bd>
2016-10-27 19:46:23 +0600

    custom filter added

--- Lecture12/app.js
+++ Lecture12/app.js
@@ -2,22 +2,38 @@
 'use strict';
 
 angular.module('MsgApp', [])
-.controller('MsgController', MsgController);
+.controller('MsgController', MsgController)
+.filter('loves', LovesFilter);
 
-MsgController.$inject = ['$scope', '$filter'];
-function MsgController($scope, $filter) {
+MsgController.$inject = ['$scope', '$filter', "lovesFilter"];
+function MsgController($scope, $filter, lovesFilter) {
   $scope.name = "Yaakov";
   $scope.stateOfBeing = "hungry";
   $scope.cookieCost = .45;
 
   $scope.sayMessage = function () {
     var msg = "Yaakov likes to eat healthy snacks at night!";
-    var output = $filter('uppercase')(msg);
-    return output;
+    //var output = $filter('uppercase')(msg);
+    return msg;
   };
+
+  $scope.sayLovesMessage = function () {
+    var msg = "Yaakov likes to eat healthy snacks at night!";
+    msg = lovesFilter(msg);
+    return msg;
+  };
+
 
   $scope.feedYaakov = function () {
     $scope.stateOfBeing = "fed";
+  };
+}
+
+function LovesFilter() {
+  return function (input) {
+    input = input || "";
+    input = input.replace("likes", "loves");
+    return input;
   };
 }
 
