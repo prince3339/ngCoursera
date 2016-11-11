c6f6a351d12f0c0406a2b88724f3cf35bf399c3a
Sabbir Ahmed <Sabbir Ahmed>
2016-11-12 02:07:20 +0600

    ass 3 completed

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -2,19 +2,49 @@
   'use strict';
   angular.module('NarrowItDownApp', [])
     .controller('NarrowItDownController', NarrowItDownController)
-    .service('MenuSearchService', MenuSearchService);
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems', foundItems);
+
+    function foundItems() {
+      var ddo = {
+        templateUrl: 'remove-item.html',
+        scope: {
+          foundMenuItems: '<',
+          onRemove: '&'
+        },
+        controller: DontNarrowDownController,
+        controllerAs: 'ctrl',
+        bindToController: true
+      }
+      return ddo;
+    }
+
+    function DontNarrowDownController () {
+      var ctrl = this;
+    }
 
     NarrowItDownController.$inject = ["MenuSearchService"];
     function NarrowItDownController (MenuSearchService) {
         var ctrl = this;
-        ctrl.searchItem = "";
 
         ctrl.narrowDown = function (search) {
+            //ctrl.searchItem = "";
+
+            ctrl.showFoundItems = [];
             var promise = MenuSearchService.getMatchedMenuItems(search);
+            ctrl.loading = true;
             promise.then(function (result) {
+              ctrl.notFoundCheck = MenuSearchService.notFoundCheck;
+              //console.log(ctrl.notFoundCheck);
               ctrl.showFoundItems = result;
-            })
+              ctrl.loading = false;
+            });
         }
+
+        ctrl.removeItem = function (index) {
+          ctrl.showFoundItems.splice(index, 1);
+        }
+
     }
 
     MenuSearchService.$inject = ["$http"];
@@ -29,21 +59,26 @@
           }).then(function(response) {
               var menuItems = response.data;
               var count = 0;
-              console.log(menuItems.menu_items);
+              //console.log(menuItems.menu_items);
               var foundItems = [];
               if(searchItem === undefined || searchItem == "") {
-                var notFoundCheck = true;
+                service.notFoundCheck = true;
+                //console.log(service.notFoundCheck);
               }else{
+                service.notFoundCheck = false;
                 for(var i=0; i<=menuItems.menu_items.length; i++) {
                   if(menuItems.menu_items[i]) {
                     var description = menuItems.menu_items[i].description.toLowerCase();
+
                     if(description.indexOf(searchItem.toLowerCase()) !==-1) {
-                        console.log("Found");
+                        // console.log(searchItem);
+                        // console.log(searchItem.toLowerCase());
+                        // console.log(description);
                         foundItems.push(menuItems.menu_items[i]);
                     }
                   }
                 }
-                console.log(foundItems);
+                //console.log(foundItems);
                 return foundItems;
               }
           }).catch(function (error) {
